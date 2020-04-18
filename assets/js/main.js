867bc2724c641c10e689bfc4000bc786d139d594
perdunovivan53 <63893123+perdunovivan53@users.noreply.github.com>
2020-04-18 16:41:57 +1200

    55

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.pets__slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        centerMode: true,
+    });
+});
+
+
+    
